9526428abbfdecb567f7265766d9b12d916fe178
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-15 03:15:52 +0530

    Linked List_add()_Linked_List_demo

---
+++ LL_add_op_LLD.java
@@ -0,0 +1,17 @@
+package javapractise;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+	LinkedList l = new LinkedList();
+	l.addFirst(5);
+	l.addFirst(15);
+	l.addFirst(25);
+	l.display();
+
+		
+
+
+	}
+
+}
