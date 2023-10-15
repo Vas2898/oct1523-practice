29d12e39d7686893cf7334584df0a36463766b42
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-15 20:43:01 +0530

    int To String Type conversion

---
+++ String_valueOf.java
@@ -0,0 +1,12 @@
+public class TypeCasting {
+	// Conversion from integer to String
+
+	public static void main(String[] args) {
+		int no = 12;
+		System.out.println("integer before casting "+no);
+		
+		String no1 = String.valueOf(no); 
+		System.out.println("After casting "+ no1);
+	}
+
+}
