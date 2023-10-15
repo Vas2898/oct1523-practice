00545bfb4c076e780df7206d437b51de75d8ee50
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-15 21:56:00 +0530

    Array_Listj

---
+++ ArrayList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class TypeCasting {
+	// Conversion from integer to String
+
+	public static void main(String[] args) {
+		ArrayList <String> animals = new ArrayList<>();
+		animals.add("dog");
+		animals.add("Lion");
+		animals.add("Tiger");
+		animals.add("Elephant");
+		System.out.println(animals);
+	}
+
+}
