fc765040b5c9e098f7789afef83c77872709060e
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-15 20:48:13 +0530

    Type conversion string to integer

---
+++ String_Int.java
@@ -0,0 +1,12 @@
+public class TypeCasting {
+	// Conversion from integer to String
+
+	public static void main(String[] args) {
+		String no = "12";
+		System.out.println("integer before casting "+no);
+		
+		int no1 = Integer.parseInt(no); 
+		System.out.println("After casting "+ no1);
+	}
+
+}
