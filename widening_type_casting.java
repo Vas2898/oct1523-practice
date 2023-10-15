a1ffe2876cef165615456fe7c7b11ac63cb7792e
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-15 20:16:21 +0530

    widening_type_casting(Implicit Type casting)

---
+++ widening_type_casting.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		int no = 12;
+		System.out.println("integer before casting"+no);
+		
+		double no1 = no;
+		System.out.println("After casting"+ no1);
+	}
+
+}
