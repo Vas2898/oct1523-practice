274b69d792f91f5894d8aee60ec8a8c1cfcf4c76
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-15 20:31:56 +0530

    Narrowing_Type_Casting or Explicit Type Casting

---
+++ narrowing_Type_Casting.java
@@ -0,0 +1,12 @@
+public class TypeCasting {
+	// Narrowing type casting or Explicit type casting
+
+	public static void main(String[] args) {
+		double no = 12;
+		System.out.println("integer before casting"+no);
+		
+		double no1 = (int)no; 
+		System.out.println("After casting"+ no1);
+	}
+
+}
